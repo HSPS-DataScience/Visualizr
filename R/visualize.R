41bc82bce35ceaba4821a2aa25b57a1a6f340fbb
johnmwood <jmw.home@gmail.com>
2018-10-15 15:53:03 -0600

    Path change

--- R/visualize.R
+++ R/visualize.R
@@ -4,7 +4,7 @@
 #' @param new_filename path to new Rmd file being created
 #' @import HSPSUtils reticulate
 #' @export
-visualize <- function(rscript_path = "R/visualize_functions.R", new_filename = "test.Rmd") {
+visualize <- function(rscript_path = "./R/visualize_functions.R", new_filename = "test.Rmd") {
   parser <- p$parser$Parser(rscript_path, new_filename)
 
   # create new Rmd file
