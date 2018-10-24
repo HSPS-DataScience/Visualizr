ba778821da99ce1dafe130bddf0527ac6d529325
JohnMWood <jmw.home@gmail.com>
2018-10-24 14:40:13 -0600

    Fixed Python versioning bug where RStudio recognizes base Python instead of Anaconda3 distributions

--- R/visualize.R
+++ R/visualize.R
@@ -1,11 +1,24 @@
-#' Generate Rmarkdown report and Rmarkdown standalone file from a single command.
+#' Generate standalone RMarkdown file from a single command based off of a pre-defined template.
 #'   This function is a wrapper around `RParser`, a Python library developed
 #'   for parsing R scripts into Rmarkdown.
+#'
+#'   IMPORTANT: Make sure anaconda3 and rparser and installed before running visualize()
 #'
 #' @param new_filename path to new Rmd file being created
 #' @import reticulate
 #' @export
 visualize <- function(new_filename = "./test.Rmd") {
+  # rparser requires Python 3.6 >=
+  tryCatch(
+    expr = use_python("/anaconda3/bin/python"),
+    error = function(e) {
+      message("Make sure Anaconda3 (/anaconda3/bin/python) \n\t and rparser (github.com/HSPS-DataScience/rparser) are installed")
+    },
+    warning = function(w) {
+      message("Make sure Anaconda3 (/anaconda3/bin/python) \n\t and rparser (github.com/HSPS-DataScience/rparser) are installed")
+    }
+  )
+
   p$run_parser(new_filename = new_filename)
 }
 
